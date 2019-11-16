9c589e9bfffa8cd1f243e49dd6a3ecd1240c37ed
toshihi07 <55124827+toshihi07@users.noreply.github.com>
2019-11-17 01:08:29 +0900

    管理者権限機能の実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_112149) do
+ActiveRecord::Schema.define(version: 2019_11_16_153145) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_112149) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.string "avatar"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
