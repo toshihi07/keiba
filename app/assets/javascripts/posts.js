476cbdf56bbb29837cc48bd2a2ace45f36385c30
toshihi07 <55124827+toshihi07@users.noreply.github.com>
2019-11-23 14:34:35 +0900

    デプロイ前

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -13,11 +13,18 @@ $(function() {
       contentType: false
     })
     .done(function(data){
-      console.log('a')
       buildHTML(data);
     })
   })
+  .fail(function() {
+    window.alert('正しい結果を得られませんでした。');
+  });
   function buildHTML(data){
+    html_count = `<h1 class='pl-3'>
+    ${data.name}(${data.year})掲示板/投稿数(${data.count}件)
+    </h1>`
+    $('#post_count').empty();
+    $('#post_count').prepend(html_count);
     html = `<div class='post-index__user-data'>
               ${data.nickname}
             </div>
@@ -31,5 +38,7 @@ $(function() {
               いいね 返信 通報
             </div>`
     $('.post-index').prepend(html);
+    $('#post_prediction').val('');
+    $('#post_body').val('');
   }
 })
--- app/views/posts/create.json.jbuilder
+++ app/views/posts/create.json.jbuilder
@@ -1,3 +1,6 @@
 json.prediction @post.prediction
 json.body @post.body
 json.nickname @post.user.nickname
+json.name @race_group.name
+json.year @race_group.year
+json.count @posts_length.count
