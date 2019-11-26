d00ffd32f010c6fa90afebeda0840418d7c57bb9
toshihi07 <55124827+toshihi07@users.noreply.github.com>
2019-11-26 18:07:22 +0900

    収支管理1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ before_action :set_group
 
   def new
     @post = Post.new
-    @posts = Post.all.limit(10).order("created_at DESC").page(params[:page]).per(20)
+    @posts = Post.all.limit(10).order("created_at ASC").page(params[:page]).per(20)
     @posts_length = @race_group.posts
   end
 
