11881b46e1ba4d69e41accc41d443c7750e58740
toshihi07 <55124827+toshihi07@users.noreply.github.com>
2019-11-29 19:26:42 +0900

    デプロイ　エラー

--- db/schema.rb
+++ db/schema.rb
@@ -124,12 +124,6 @@ ActiveRecord::Schema.define(version: 2019_11_27_101511) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "results", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "result"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
